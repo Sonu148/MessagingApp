6217d110980329fa7843215ccaca5dce02137970
Sonu Kumar <sonukumar31815@gmail.com>
2025-02-21 22:16:05 +0530

    [Sonu]Add.messaging with query paramater in UC2

--- src/main/java/com/training/hellomessagingapp/controlller/MessageAppController.java
+++ src/main/java/com/training/hellomessagingapp/controlller/MessageAppController.java
@@ -12,4 +12,9 @@ public class MessageAppController {
         return "Hello from BridgeLabz!!!";
     }
 
+    // Method for the hello message with name
+    @GetMapping("/hello/query")
+    public String sayHelloQuery(@RequestParam (value= "name") String name){
+        return "Hello "+name+" from BridgeLabz!!!";
+    }
 }
