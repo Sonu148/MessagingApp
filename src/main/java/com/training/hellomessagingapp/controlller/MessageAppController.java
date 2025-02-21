9612764dce38e0b51fa186bea9ba646f34b04945
Sonu Kumar <sonukumar31815@gmail.com>
2025-02-21 23:28:25 +0530

    [Sonu]Add. get full name from json using post request in message

--- src/main/java/com/training/hellomessagingapp/controlller/MessageAppController.java
+++ src/main/java/com/training/hellomessagingapp/controlller/MessageAppController.java
@@ -1,4 +1,5 @@
 package com.training.hellomessagingapp.controlller;
+import com.training.hellomessagingapp.UserData;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,5 +21,13 @@ public class MessageAppController {
     @RequestMapping("/hello/path/{name}")
     public String hello(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz!";
+    }
+
+    // Method for handling POST request with request body
+    @PostMapping("/hello/post")
+    public String hello(@RequestBody UserData user) {
+        String firstName = user.getFirstName();
+        String lastName = user.getLastName();
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
     }
 }
